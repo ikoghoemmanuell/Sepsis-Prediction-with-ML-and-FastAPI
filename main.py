bc2e8a8ba1b99098d4fee1d16f42ca78730b42df
ikoghoemmanuell <102419217+ikoghoemmanuell@users.noreply.github.com>
2023-05-30 10:45:59 +0100

    restructured and added informative print statements.

--- main.py
+++ main.py
@@ -5,19 +5,63 @@ import pandas as pd
 import numpy as np
 import logging
 import uvicorn
+import os
 
 # Configure logger
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+# API
+app = FastAPI(title="API")
+
+# Util Functions & Classes
 
 # Load the model and scaler
-model = pickle.load(open("model.pkl", "rb"))
-scaler = pickle.load(open("scaler.pkl", "rb"))
+def load_model():
+    with open("model.pkl", "rb") as f:
+        model = pickle.load(f)
+    return model
 
-# Define the input data model using Pydantic
-class InputData(BaseModel):
+def load_scaler():
+    with open("scaler.pkl", "rb") as f:
+        scaler = pickle.load(f)
+    return scaler
+
+model = load_model()
+scaler = load_scaler()
+
+def predict(df, endpoint="simple"):
+    """Take a dataframe as input and use it to make predictions"""
+    print(f"[INFO] 'predict' function has been called through the endpoint: {endpoint}")
+    print(f"[INFO] Input dataframe:\n{df}")
+
+    # Scaling
+    scaled_df = scaler.transform(df)
+    print(f"INFO: Scaled dataframe:\n{scaled_df}")
+
+    # Prediction
+    prediction = model.predict_proba(scaled_df)
+    print(f"INFO: Prediction probabilities:\n{prediction}")
+
+    # Formatting of the prediction
+    highest_proba = prediction.max(axis=1)
+    print(f"INFO: Highest probabilities: {highest_proba}")
+
+    highest_proba_idx = prediction.argmax(axis=1)
+    print(f"INFO: Highest probability indexes: {highest_proba_idx}")
+
+    predicted_classes = [labels[i] for i in highest_proba_idx]
+    print(f"INFO: Predicted classes: {predicted_classes}")
+
+    df["predicted_proba"] = highest_proba
+    df["predicted_label"] = predicted_classes
+    print(f"INFO: DataFrame with prediction:\n{df}")
+
+    parsed = df.to_dict('records')
+    return parsed
+
+
+class Patient(BaseModel):
     Blood_Work_R1: int
     Blood_Pressure: int
     Blood_Work_R3: int
@@ -25,37 +69,41 @@ class InputData(BaseModel):
     Blood_Work_R4: float
     Patient_age: int
 
-# Convert the input values to a pandas DataFrame with appropriate column names
-def input_df_creator(data: InputData):
-    input_data = pd.DataFrame(data.dict(), index=[0])
-    return input_data
 
-# Define the sepsis prediction endpoint
-@app.post("/predict_sepsis")
-def predict_sepsis(data: InputData):
-    input_df = input_df_creator(data)
+class Patients(BaseModel):
+    all: list[Patient]
+    
+    @classmethod
+    def return_list_of_dict(cls):
+        return [i.dict() for i in cls.all]
 
-    # Scale numerical variables
-    num_cols = input_df.select_dtypes(include=['int64', 'float64']).columns
-    num_scaled = scaler.transform(input_df)
 
-    processed_df = pd.DataFrame(num_scaled, columns=num_cols)
+labels = load_model().classes_
 
-    # Make prediction
-    prediction = model.predict(processed_df)
-    sepsis_prediction = bool(prediction[0])
-
-    return {"prediction": sepsis_prediction}
-
-# Root endpoint
+# Endpoints
 @app.get("/")
 def root():
     return {"API": "This is an API for sepsis prediction."}
 
-# Checkup endpoint
 @app.get("/checkup")
 def checkup(a: int = None, b: int = 0):
     return {"a": a, "b": b}
+
+## ML endpoint
+@app.post("/predict")
+def predict_sepsis(patient: Patient):
+    """Make prediction with the passed data"""
+    data = pd.DataFrame(patient.dict(), index=[0])
+    parsed = predict(df=data)
+    return {"output": parsed}
+
+@app.post("/predict_multi")
+def predict_sepsis_for_multiple_patients(patients: Patients):
+    """Make prediction with the passed data"""
+    data = pd.DataFrame(patients.return_list_of_dict())
+    parsed = predict(df=data, endpoint="multi")
+    return {"output": parsed}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
