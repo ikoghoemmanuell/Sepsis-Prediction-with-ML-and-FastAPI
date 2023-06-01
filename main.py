6e18dbeea106e7f31de87fc4d241237cd2732470
ikoghoemmanuell <oke.kogo@gmail.com>
2023-06-01 17:35:52 +0100

    cleaned up fastAPI by removing unnecessary things

--- main.py
+++ main.py
@@ -3,58 +3,37 @@ from pydantic import BaseModel
 import pickle
 import pandas as pd
 import numpy as np
-import logging
 import uvicorn
 import os
 
-# Configure logger
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-# API
+# call the app
 app = FastAPI(title="API")
 
-# Util Functions & Classes
-
 # Load the model and scaler
-def load_model():
-    with open("model.pkl", "rb") as f:
-        model = pickle.load(f)
-    return model
+def load_model_and_scaler():
+    with open("model.pkl", "rb") as f1, open("scaler.pkl", "rb") as f2:
+        return pickle.load(f1), pickle.load(f2)
 
-def load_scaler():
-    with open("scaler.pkl", "rb") as f:
-        scaler = pickle.load(f)
-    return scaler
-
-model = load_model()
-scaler = load_scaler()
+model, scaler = load_model_and_scaler()
 
 def predict(df, endpoint="simple"):
-    """Take a dataframe as input and use it to make predictions"""
-    print(f"[INFO] 'predict' function has been called through the endpoint: {endpoint}")
-    print(f"[INFO] Input dataframe:\n{df}")
-
     # Scaling
     scaled_df = scaler.transform(df)
 
     # Prediction
     prediction = model.predict_proba(scaled_df)
-    print(f"INFO: Prediction probabilities:\n{prediction}")
 
-    # Formatting of the prediction
     highest_proba = prediction.max(axis=1)
 
-    highest_proba_idx = prediction.argmax(axis=1)
-
-    predicted_labels = ["Negative" if i == 0 else "Positive" for i in highest_proba_idx]
-    print(f"INFO: Predicted labels: {predicted_labels}")
+    predicted_labels = ["Patient does not have sepsis" if i == 0 else f"Patient has sepsis" for i in highest_proba]
+    print(f"Predicted labels: {predicted_labels}")
+    print(highest_proba)
 
     response = []
     for label, proba in zip(predicted_labels, highest_proba):
         output = {
-            "predicted_proba": proba,
-            "predicted_label": label
+            "prediction": label,
+            "probability of prediction": str(round(proba * 100)) + '%'
         }
         response.append(output)
 
@@ -79,28 +58,28 @@ class Patients(BaseModel):
             patient_dict = patient.dict()
             patient_list.append(patient_dict)
         return patient_list
-
-
-labels = load_model().classes_
-
+    
 # Endpoints
+# Root Endpoint
 @app.get("/")
 def root():
     return {"API": "This is an API for sepsis prediction."}
 
-@app.get("/checkup")
-def checkup(a: int = None, b: int = 0):
-    return {"a": a, "b": b}
-
-## Prediction endpoint
+# Prediction endpoint
 @app.post("/predict")
+def predict_sepsis(patient: Patient):
+    # Make prediction
+    data = pd.DataFrame(patient.dict(), index=[0])
+    parsed = predict(df=data)
+    return {"output": parsed}
+
+# Multiple Prediction Endpoint
+@app.post("/predict_multiple")
 def predict_sepsis_for_multiple_patients(patients: Patients):
     """Make prediction with the passed data"""
     data = pd.DataFrame(Patients.return_list_of_dict(patients))
     parsed = predict(df=data, endpoint="multi")
     return {"output": parsed}
-
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
